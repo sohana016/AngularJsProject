56cedcc91787804f12b54e0e8635485198b3b3c5
Tawhida Shorif <tawhida016@gmail.com>
2019-10-06 15:02:12 +0600

    dashbord

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 var app = angular.module("myApp",[ngRoute]);
+
+app.config(function($routeProvider) { // configure for routing
+    $routeProvider.when("/", {
+        templateUrl: "homePage.html",
+        controller: "authController"
+    }).when("/login", {
+        templateUrl: "src/login.html",
+        controller: "authController"
+    })
+    .when("/signUp", {
+        templateUrl: "src/signUp.html",
+        controller: "authController"
+    })
+});
 app.controller("authController" ,function($scope){
     $scope.login = function(){
         if($scope.userId =="abc" && $scope.password == "123"){
-           console.log("hdgfhdg");
+        //    console.log("hdgfhdg");
+        window.location.href="/homePage.html"
         }
     }
     
